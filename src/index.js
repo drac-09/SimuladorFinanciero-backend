302b3aa051711fb15db12cd2e858faf94a1e0842
drac-09 <allan.estrada9@gmail.com>
2023-12-04 20:25:10 -0600

    Actualizando

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 import { connectDB } from "./modules/database-module.js";
 import { PORT } from "./config.js";
 
-connectDB(); //Conexion a la Base de Datos
-app.listen(PORT);
+connectDB(); //ConexiÓn a la Base de Datos
+app.listen(PORT || 5000); // Puerto para levantar el servidor
 console.log("Server on port: ", PORT); // Levantar el servidor
