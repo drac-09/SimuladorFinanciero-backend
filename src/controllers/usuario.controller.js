04c56fb1febd5ee98550591290ce69a4414ca2ed
drac-09 <allan.estrada9@gmail.com>
2024-01-17 18:12:19 -0600

    Agregando cabecera por CORS

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -36,6 +36,7 @@ export const registro = async (req, res) => {
 };
 
 export const acceso = async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   const { correo, contrasenia } = req.body;
   try {
     const usuarioEncontrado = await Usuario.findOne({ correo });
