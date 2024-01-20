6c7b2b217d8ce8b3b6b852716719fc9b7bb54b60
drac-09 <allan.estrada9@gmail.com>
2024-01-19 20:05:19 -0600

    Modificando CORS2

--- src/app.js
+++ src/app.js
@@ -7,29 +7,30 @@ import usuarioRoutes from "./routers/usuario.routes.js";
 import escenarioRoutes from "./routers/escenario.routes.js";
 
 const app = express();
-const options = [
-  cors({
-    origin: "*",
-    methods: "*",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
+const lista = [
+  `http://localhost:${process.env.PORT_FRONTEND}`,
+  "https://simulador-financiero-frontend.vercel.app/",
+  "https://vercel.com/drac-09/simulador-financiero-frontend/E5wgczVnXqeruEx8QQhmRV7fVgVF",
 ];
 
-app.use(options);
-// const lista = [
-//   `http://localhost:${process.env.PORT_FRONTEND}`,
-//   "https://simulador-financiero-frontend.vercel.app/",
-//   "https://vercel.com/drac-09/simulador-financiero-frontend/E5wgczVnXqeruEx8QQhmRV7fVgVF",
-// ];
+// Middlewares
+app.use(
+  cors({
+    origin: lista,
+    credentials: true,
+  })
+);
 
-// // Middlewares
-// app.use(
-//   cors({
-//     origin: lista,
-//     credentials: true,
-//   })
-// );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  next();
+});
 
 app.use(morgan("dev"));
 app.use(cookieParser());
