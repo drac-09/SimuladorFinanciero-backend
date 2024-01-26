68883dc9c74071b6f393de02af8bb27231ff4c38
drac-09 <allan.estrada9@gmail.com>
2024-01-25 22:03:41 -0600

    Prueba

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -43,11 +43,11 @@ export const acceso = async (req, res) => {
     if (!usuarioEncontrado)
       return res.status(400).json(["Usuario no Encontrado"]);
 
-    const comparar = await bcrypt.compare(
-      contrasenia,
-      usuarioEncontrado.contrasenia
-    );
-    if (!comparar) return res.status(400).json(["Error en Contraseña"]);
+    // const comparar = await bcrypt.compare(
+    //   contrasenia,
+    //   usuarioEncontrado.contrasenia
+    // );
+    // if (!comparar) return res.status(400).json(["Error en Contraseña"]);
     // const token = await crearAccesoToken({ id: usuarioEncontrado._id });
     // res.cookie("token", token);
     res.json({
