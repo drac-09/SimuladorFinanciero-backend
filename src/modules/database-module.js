614672eb9ed05f8156c7981b9c702713cecb16cc
drac-09 <allan.estrada9@gmail.com>
2024-01-25 22:48:14 -0600

    Actualizando enlace a mongoDB

--- src/modules/database-module.js
+++ src/modules/database-module.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.URL_DB}`);
+    await mongoose.connect(process.env.URL_DB);
     console.log(">>> BD is connected...");
   } catch (error) {
     console.log(error);
