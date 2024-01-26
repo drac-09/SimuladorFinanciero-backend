1675c7f791a139de14888fa1a3f22f1f36f40966
drac-09 <allan.estrada9@gmail.com>
2024-01-25 21:21:12 -0600

    Probando servicio-acceso

--- src/routers/usuario.routes.js
+++ src/routers/usuario.routes.js
@@ -14,7 +14,7 @@ import { registroSchema, accesoSchema } from "../schemas/usuario.schema.js";
 const router = Router();
 
 router.post("/registro", validarSchema(registroSchema), registro);
-router.post("/acceso", acceso);
+router.post("/acceso", validarSchema(accesoSchema), acceso);
 router.post("/cerrar", cerrar);
 router.get("/prueba", prueba);
 // router.get("/verify", verifyToken);
