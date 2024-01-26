85c0a58a165b420ad3d38bb61db0fc8eef642d7f
drac-09 <allan.estrada9@gmail.com>
2024-01-26 13:07:26 -0600

    Verificando que se recibio el token

--- src/middlewares/validarToken.js
+++ src/middlewares/validarToken.js
@@ -3,7 +3,10 @@ import jwt from "jsonwebtoken";
 export const autenticacionRequerida = (req, res, next) => {
   const { token } = req.cookies;
   // console.log(req);
-  if (!token) return res.status(401).json(req);
+  if (!token)
+    return res.status(401).json({
+      message: `No existe el token, autorización denegada ${req}`,
+    });
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, usuario) => {
     if (err) return res.status(403).json({ message: "Token Invalido" });
