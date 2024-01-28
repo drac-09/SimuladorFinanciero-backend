4448efb823d1ed3b68666c4bdb05f22ef0f4a30c
drac-09 <allan.estrada9@gmail.com>
2024-01-28 16:36:32 -0600

    Actualizando

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use("/api/", usuarioRoutes);
 app.use("/api/", escenarioRoutes);
 
 //Mensaje
-app.get("/api/", function (req, res) {
+app.get("/", function (req, res) {
   res.send("Servidor Funcionando");
 });
 
