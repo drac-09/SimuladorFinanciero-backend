cf527b9c834f10340283967a263d17fa1161748f
drac-09 <allan.estrada9@gmail.com>
2024-01-28 15:25:17 -0600

    Actualizando

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ app.use(
   cors({
     origin: lista,
     credentials: true,
+    // methods: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    // allowedHeaders: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
   })
 );
 
